d94e61963036e6a9768acc810c63122df23001f1
kevin-alexis <kevinlevin367@outlook.com>
2024-06-04 10:27:59 -0500

    Gifs

---
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -0,0 +1,31 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
+
+@Component({
+  selector: 'gifs-search-box',
+  template:`
+  <h5>Buscar:</h5>
+  <input
+  (keyup.enter)="searchTag()"
+  #txtTagInput
+  type="text"
+  class="form-control"
+  placeholder="Buscar gifs...">`
+})
+
+export class SearchBoxComponent {
+
+  constructor( private gifsService: GifsService){
+
+  }
+
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
+
+  searchTag( ){
+    const newTag = this.tagInput.nativeElement.value;
+    this.gifsService.searchTag(newTag);
+    this.tagInput.nativeElement.value = '';
+
+  }
+}
